3acbacbddfa1dd0d4f8b25c921497def59d73350
unknown <antonio.cortes.sotelo@gmail.com>
2022-11-20 23:25:36 -0300

    ok

--- render/static/js/login.js
+++ render/static/js/login.js
@@ -1,33 +1,48 @@
 const btnTrabajador = document.getElementById('logTrabajador');
 const btnEmpleador = document.getElementById('logEmpleador');
 
-
 btnEmpleador.onclick = function switchEmpleador() {
     const cuerpoLoginTrabajador = document.getElementById("LogTrabajadorForm");
     cuerpoLoginTrabajador.style.display = 'none';
+    const registrarEmpleadorForm = document.getElementById('registrarEmpleadorForm');
+    registrarEmpleadorForm.style.display = 'none'
+    const registrarTrabajadorForm = document.getElementById('registrarTrabajadorForm');
+    registrarTrabajadorForm.style.display = 'none'
 
     const cuerpoLogin = document.getElementById("logEmpleadorForm");
     cuerpoLogin.style.display = 'block';
 
-    btnEmpleador.style.backgroundColor = "gray"
-    btnTrabajador.style.backgroundColor = "transparent"
+    btnEmpleador.style.border = "1px solid #E5E5E5"
+    btnTrabajador.style.border = "none"
 }
 
 btnTrabajador.onclick = function switchTrabajador() {
     const cuerpoLoginEmpleador = document.getElementById("logEmpleadorForm");
     cuerpoLoginEmpleador.style.display = 'none';
+    const registrarEmpleadorForm = document.getElementById('registrarEmpleadorForm');
+    registrarEmpleadorForm.style.display = 'none'
+    const registrarTrabajadorForm = document.getElementById('registrarTrabajadorForm');
+    registrarTrabajadorForm.style.display = 'none'
 
     const cuerpoLogin = document.getElementById("LogTrabajadorForm");
     cuerpoLogin.style.display = 'block';
-    btnTrabajador.style.backgroundColor = "gray"
-    btnEmpleador.style.backgroundColor = "transparent"
+    btnTrabajador.style.border = "1px solid #E5E5E5"
+    btnEmpleador.style.border = "none"
 }
 
 const registrarTrabajador = document.getElementById('registrarTrabajador');
 registrarTrabajador.onclick = function showRegistroTrabajador() {
     const cuerpoLoginTrabajador = document.getElementById("LogTrabajadorForm");
-    cuerpoLoginTrabajador.style.display = 'none'; 
+    cuerpoLoginTrabajador.style.display = 'none';
     const registrarTrabajadorForm = document.getElementById('registrarTrabajadorForm');
     registrarTrabajadorForm.style.display = 'block'
 
+}
+
+const registrarEmpleador = document.getElementById('registrarEmpleador');
+registrarEmpleador.onclick = function showRegistroEmpleador() {
+    const logEmpleadorForm = document.getElementById("logEmpleadorForm");
+    logEmpleadorForm.style.display = 'none';
+    const registrarEmpleadorForm = document.getElementById('registrarEmpleadorForm');
+    registrarEmpleadorForm.style.display = 'block'
 }
